63b43c4e839b879cbb0c1b8ec529626931825ee9
Dark-Floyd <zoharzaborski@gmail.com>
2022-01-24 12:54:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tvshowapp/Activity_Login.java
@@ -0,0 +1,4 @@
+package com.example.tvshowapp;
+
+public class Activity_Login {
+}
---
+++ app/src/main/java/com/example/tvshowapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tvshowapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
+    }
+}
