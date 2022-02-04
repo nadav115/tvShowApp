7415e09dab8bc20ad534274808421fbb3b42ea57
Zohar Zaborski <204124382@idf.il>
2022-02-04 14:52:05 +0200

    updated DB

--- app/src/main/java/com/example/tvshowapp/MainActivity.java
+++ app/src/main/java/com/example/tvshowapp/MainActivity.java
@@ -1,7 +1,19 @@
 package com.example.tvshowapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
+import java.util.Random;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
---
+++ app/src/main/java/com/example/tvshowapp/tvShow.java
@@ -0,0 +1,68 @@
+package com.example.tvshowapp;
+
+public class tvShow {
+    private String title;
+    private String releaseDate;
+    private String genre;
+    private String length;
+    private int seasons;
+    private int episodes;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        this.length = length;
+    }
+
+    public int getSeasons() {
+        return seasons;
+    }
+
+    public void setSeasons(int seasons) {
+        this.seasons = seasons;
+    }
+
+    public int getEpisodes() {
+        return episodes;
+    }
+
+    public void setEpisodes(int episodes) {
+        this.episodes = episodes;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    private int rating;
+}
