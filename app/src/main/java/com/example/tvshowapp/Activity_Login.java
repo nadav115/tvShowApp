3e28122610fbcf4719b197dbaa0ca74b99b96ddb
Zohar Zaborski <204124382@idf.il>
2022-02-06 10:20:34 +0200

    updated signup

--- app/src/main/java/com/example/tvshowapp/Activity_Login.java
+++ app/src/main/java/com/example/tvshowapp/Activity_Login.java
@@ -1,4 +1,78 @@
 package com.example.tvshowapp;
 
-public class Activity_Login {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Activity_Login extends AppCompatActivity {
+    private EditText user_name, pass_word;
+    private MaterialButton btn_login;
+    private MaterialButton btn_sign;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        user_name=findViewById(R.id.email_input);
+        pass_word=findViewById(R.id.password_input);
+        btn_login = findViewById(R.id.login_btn);
+        btn_sign = findViewById(R.id.register_btn);
+        mAuth=FirebaseAuth.getInstance();
+        btn_login.setOnClickListener(v -> {
+            String email= user_name.getText().toString().trim();
+            String password=pass_word.getText().toString().trim();
+            if(email.isEmpty())
+            {
+                user_name.setError("Email is empty");
+                user_name.requestFocus();
+                return;
+            }
+            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+            {
+                user_name.setError("Enter the valid email");
+                user_name.requestFocus();
+                return;
+            }
+            if(password.isEmpty())
+            {
+                pass_word.setError("Password is empty");
+                pass_word.requestFocus();
+                return;
+            }
+            if(password.length()<6)
+            {
+                pass_word.setError("Length of password is more than 6");
+                pass_word.requestFocus();
+                return;
+            }
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(task -> {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(Activity_Login.this,
+                            "Connected!",
+                            Toast.LENGTH_SHORT).show();
+                    //startActivity(new Intent(Activity_Login.this, Activity_SignUp.class));
+                    // activity check
+                }
+                else
+                {
+                    Toast.makeText(Activity_Login.this,
+                            "Please Check Your login Credentials",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+            });
+        });
+        btn_sign.setOnClickListener(v -> startActivity(new Intent(Activity_Login.this,Activity_SignUp.class )));
+    }
+
 }
--- app/src/main/java/com/example/tvshowapp/MainActivity.java
+++ app/src/main/java/com/example/tvshowapp/MainActivity.java
@@ -1,25 +1,100 @@
-package com.example.tvshowapp;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import java.util.ArrayList;
-import java.util.Random;
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.activity_main);
-    }
-}
+//package com.example.tvshowapp;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.annotation.NonNull;
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.os.Bundle;
+//import android.util.Log;
+//import android.widget.TextView;
+//import com.google.firebase.database.DataSnapshot;
+//import com.google.firebase.database.DatabaseError;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.ValueEventListener;
+//import java.util.ArrayList;
+//import java.util.Random;
+//import android.os.Bundle;
+//
+//public class MainActivity extends AppCompatActivity {
+//// need to add implements here
+//
+//    private LoginFragment loginFragment;
+//    //private WelcomeFragment welcomeFragment;
+//    private SignUpFragment signUpFragment;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        findViewsId();
+//        initFragmentClasses();
+//        initCallBacks();
+//
+//        displayFragment(welcomeFragment);
+//
+//
+//    }
+//
+//    private void initCallBacks() {
+//        //welcomeFragment.initLogInRegisterCallBack(this);
+//        signUpFragment.initNewAccountCallBack(this);
+//        //welcomeFragment.initOpenParkingActivityCallBack(this);
+//        loginFragment.initOpenParkingActivityCallBack(this);
+//    }
+//
+//    private void initFragmentClasses() {
+//        loginFragment = new LoginFragment();
+//        welcomeFragment = new WelcomeFragment();
+//        signUpFragment = new RegisterFragment();
+//
+//    }
+//
+//    private void displayFragment(Fragment fragment) {
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        fragmentManager.beginTransaction().setCustomAnimations(
+//                R.anim.slide_in,  // enter
+//                R.anim.fade_out,  // exit
+//                R.anim.fade_in,   // popEnter
+//                R.anim.slide_out  // popExit
+//        )
+//                .replace(R.id.frameLayout, fragment, null)
+//                .setReorderingAllowed(true)
+//                .addToBackStack(null)
+//                .commit();
+//    }
+//
+//    @Override
+//    public void onBackPressed() {
+//
+//        int count = getSupportFragmentManager().getBackStackEntryCount();
+//        //when have one fragment in backStack
+//        if (count == 1) {
+//            finish();
+//        } else {
+//            super.onBackPressed();
+//        }
+//    }
+//
+//    private void findViewsId() {
+//
+//    }
+//
+//    @Override
+//    public void logInPressBtnCallBack() {
+//        displayFragment(loginFragment);
+//    }
+//
+//    @Override
+//    public void registerPressCallBack() {
+//        displayFragment(signUpFragment);
+//
+//    }
+//
+//    @Override
+//    public void newAccountRegisterCallBack() {
+//        Intent intent = new Intent(FirstActivity.this, ParkingActivity.class);
+//        startActivity(intent);
+//        finish();
+//    }
+//}
